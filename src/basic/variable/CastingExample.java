b6c0104475ebed92e42c74d4c8d1fe3eafb9f1d1
rara-dev <kksr1219@gmail.com>
2022-11-07 17:19:16 +0900

    study casting

---
+++ src/basic/variable/CastingExample.java
@@ -0,0 +1,19 @@
+package basic.variable;
+
+public class CastingExample {
+
+	public static void main(String[] args) {
+		int intValue = 44032;
+		char charValue = (char)intValue;
+		System.out.println(charValue);    //가
+
+		long longValue = 500;
+		intValue = (int)longValue;
+		System.out.println(intValue);    //500
+
+		double doubleValue = 3.14;
+		intValue = (int)doubleValue;
+		System.out.println(intValue);    //3
+	}
+
+}
