13866b3379fbe75497cb37ec4ea21b1c01dbbd1e
rara-dev <kksr1219@gmail.com>
2022-11-07 16:46:18 +0900

    study Escape String

---
+++ src/basic/variable/EscapeExample.java
@@ -0,0 +1,13 @@
+package basic.variable;
+
+public class EscapeExample {
+
+	public static void main(String[] args) {
+		System.out.println("번호\t 이름\t 직업");             // 번호    이름    직업
+		System.out.println("행 단위 출력\n");               // 행 단위 출력 -> 아래 한줄 공백
+		System.out.println("행 단위 출력\n");               // 행 단위 출력 -> 아래 한줄 공백
+		System.out.println("우리는 \"개발자\" 입니다.");    // 우리는 "개발자" 입니다.
+		System.out.println("봄\\여름\\가을\\겨울");   // 봄\여름\가을\겨울
+	}
+
+}
