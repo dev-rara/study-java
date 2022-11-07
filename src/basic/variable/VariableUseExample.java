8fe4c5dc43feaa15dcdf1d8e694bc4d61120cb72
rara-dev <kksr1219@gmail.com>
2022-11-07 15:53:44 +0900

    study VariableUseMethod

--- src/basic/variable/VariableUseExample.java
+++ src/basic/variable/VariableUseExample.java
@@ -5,7 +5,10 @@ public class VariableUseExample {
 	public static void main(String[] args) {
 		int hour = 3;
 		int minute = 5;
-		System.out.println(hour + "시간 " + minute + "분");
+		System.out.println(hour + "시간 " + minute + "분");   //3시간 5분 출력
+
+		int totalMinute = (hour + 60) + minute;
+		System.out.println("총 " + totalMinute + "분");      //총 68분 출력
 	}
 
 }
