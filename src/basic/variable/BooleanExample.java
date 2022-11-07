6cae9d80625d873f8d7150b17470ecf605504874
rara-dev <kksr1219@gmail.com>
2022-11-07 16:56:33 +0900

    study BooleanType Variable

---
+++ src/basic/variable/BooleanExample.java
@@ -0,0 +1,16 @@
+package basic.variable;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		boolean stop = true;
+		if(stop) {
+			System.out.println("중지합니다.");
+		} else {
+			System.out.println("시작합니다.");
+		}
+
+		// 중지합니다. 출력
+	}
+
+}
