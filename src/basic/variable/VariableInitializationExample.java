c751c3963b80bcd32f2ab1a787f237a8c5afe63c
rara-dev <kksr1219@gmail.com>
2022-11-07 15:40:18 +0900

    feat: study about VariableInitialization

--- src/basic/variable/VariableInitializationExample.java
+++ src/basic/variable/VariableInitializationExample.java
@@ -4,9 +4,9 @@ public class VariableInitializationExample {
 
 	public static void main(String[] args) {
 		int value;
-		int result = value + 10;
-		// 변수 value를 선언만 하고 초기화하지 않았으므로 컴파일에러 발생
-
-		System.out.println(result);
+//		int result = value + 10;
+//		// 변수 value를 선언만 하고 초기화하지 않았으므로 컴파일에러 발생
+//
+//		System.out.println(result);
 	}
 }
