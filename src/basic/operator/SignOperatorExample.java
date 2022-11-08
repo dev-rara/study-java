fe4d94e25a80da4d8491102c43c695a0a220da13
rara-dev <kksr1219@gmail.com>
2022-11-08 15:30:43 +0900

    study SignOperator

--- src/basic/operator/SignOperatorExample.java
+++ src/basic/operator/SignOperatorExample.java
@@ -10,7 +10,7 @@ public class SignOperatorExample {
 		System.out.println("result2: " + result2);    //result3: 100
 
 		byte b = 100;
-//		byte result3 = -b;     byte타입의 연산결과는 int타입,컴파일 에러 방생
+//		byte result3 = -b;      byte타입의 연산결과는 int타입,컴파일 에러 방생
 		int result3 = -b;
 		System.out.println("result3: " + result3);    //result3: -100
 	}
