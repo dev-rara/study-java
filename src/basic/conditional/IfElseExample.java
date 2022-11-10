f94142f020dffc190834e26004fd71832e55f3f7
rara-dev <kksr1219@gmail.com>
2022-11-10 15:29:28 +0900

    study if-else statements

--- src/basic/conditional/IfElseExample.java
+++ src/basic/conditional/IfElseExample.java
@@ -8,7 +8,7 @@ public class IfElseExample {
 		if(score >= 90) {
 			System.out.println("점수가 90보다 큽니다.");
 			System.out.println("등급은 A입니다.");
-		} else {
+		} else {    //score < 90 일경우
 			System.out.println("점수가 90보다 작습니다.");
 			System.out.println("등급은 B입니다.");
 		}
