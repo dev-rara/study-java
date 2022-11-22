0a3ed21c28c4976239caa070bb8b71339f51654b
rara-dev <kksr1219@gmail.com>
2022-11-22 23:12:26 +0900

    study class

---
+++ src/basic/_class/Student.java
@@ -0,0 +1,5 @@
+package basic._class;
+
+public class Student {
+
+}
---
+++ src/basic/_class/StudentExample.java
@@ -0,0 +1,12 @@
+package basic._class;
+
+public class StudentExample {
+
+	public static void main(String[] args) {
+		Student student1 = new Student();
+		System.out.println("student1 변수가 Student 객체를 참조합니다.");
+
+		Student student2 = new Student();
+		System.out.println("student2 변수가 또 다른 Student 객체를 참조합니다.");
+	}
+}
