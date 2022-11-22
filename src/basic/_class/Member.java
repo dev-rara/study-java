9d06922e1e1580328e9d6b4e56fa265dd6954ba9
rara-dev <kksr1219@gmail.com>
2022-11-23 00:14:52 +0900

    study class

---
+++ src/basic/_class/Member.java
@@ -0,0 +1,9 @@
+package basic._class;
+
+public class Member {
+
+	String name;
+	String id;
+	String password;
+	int age;
+}
---
+++ src/basic/_class/MemberExample.java
@@ -0,0 +1,11 @@
+package basic._class;
+
+public class MemberExample {
+
+	public static void main(String[] args) {
+		Member member = new Member();
+		member.name = "김소라";
+		member.age = 33;
+	}
+
+}
