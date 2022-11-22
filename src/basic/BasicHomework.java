1f3e242af9062a33530d7629ec932b184c3c3a99
rara-dev <kksr1219@gmail.com>
2022-11-22 12:58:19 +0900

    Java Basic Homework

--- src/basic/BasicHomework.java
+++ src/basic/BasicHomework.java
@@ -7,6 +7,7 @@ public class BasicHomework {
 		int var1 = 5;
 		int var2 = 2;
 		double var3 = var1 / var2;
+		System.out.println(var3);
 		int var4 = (int) (var3 * var2);
 		System.out.println(var4);
 
