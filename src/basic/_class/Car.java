f662da7b7c0058a7e7d270b1eecc00fc357c65e1
rara-dev <kksr1219@gmail.com>
2022-11-30 10:41:15 +0900

    study access modifier

--- src/basic/_class/Car.java
+++ src/basic/_class/Car.java
@@ -4,6 +4,30 @@ public class Car {
 	String company = "현대자동차";
 	String model = "그랜저";
 	String color = "검정";
-	int maxSpeed = 350;
-	int speed;
+	final static int maxSpeed = 350;
+	private int speed;
+
+	private boolean stop;
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		if (speed < 0) {
+			this.speed = 0;
+			return;
+		} else {
+			this.speed = speed;
+		}
+	}
+
+	public boolean isStop() {
+		return stop;
+	}
+
+	public void setStop(boolean stop) {
+		this.stop = stop;
+		this.speed = 0;
+	}
 }
--- src/basic/_class/CarExample.java
+++ src/basic/_class/CarExample.java
@@ -8,11 +8,28 @@ public class CarExample {
 		System.out.println("제작회사: " + myCar.company);
 		System.out.println("모델명: " + myCar.model);
 		System.out.println("색상: " + myCar.color);
-		System.out.println("최고속도: " + myCar.maxSpeed);
-		System.out.println("현재속도: " + myCar.speed);
+		System.out.println("최고속도: " + Car.maxSpeed);
+		System.out.println("현재속도: " + myCar.getSpeed());
 
-		myCar.speed = 60;
-		System.out.println("수정된 속도: " + myCar.speed);
+		myCar.setSpeed(60);
+		System.out.println("수정된 속도: " + myCar.getSpeed());
+
+		System.out.println("======== getter & setter ========");
+		Car car = new Car();
+
+		//잘못된 속도 변경
+		car.setSpeed(-50);
+		System.out.println("현재속도: " + car.getSpeed());
+
+		//올바른 속도 변경
+		car.setSpeed(60);
+		System.out.println("현재속도: " + car.getSpeed());
+
+		//멈춤
+		if(!car.isStop()) {
+			car.setStop(true);
+		}
+		System.out.println("현재속도: " + car.getSpeed());
 	}
 
 }
