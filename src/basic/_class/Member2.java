f4af81e3161abf719ddd1cbc980491f66b18acc1
rara-dev <kksr1219@gmail.com>
2022-11-30 16:00:45 +0900

    study class

---
+++ src/basic/_class/Member2.java
@@ -0,0 +1,14 @@
+package basic._class;
+
+public class Member2 {
+
+	String name;
+	String id;
+	String password;
+	int age;
+
+	Member2(String name, String id) {
+		this.name = name;
+		this.id = id;
+	}
+}
---
+++ src/basic/_class/Member2Example.java
@@ -0,0 +1,11 @@
+package basic._class;
+
+public class Member2Example {
+
+	public static void main(String[] args) {
+		Member2 member1 = new Member2("김소라", "rara");
+		System.out.println(member1.name);
+		System.out.println(member1.id);
+	}
+
+}
