1ebf0a5b38bfcab1a00cd3476d4b5d5555a0f6f6
rara-dev <kksr1219@gmail.com>
2022-11-30 17:39:49 +0900

    study method

---
+++ src/basic/_class/Computer.java
@@ -0,0 +1,20 @@
+package basic._class;
+
+public class Computer {
+
+	int sum1(int[] values) {
+		int sum = 0;
+		for (int value : values) {
+			sum += value;
+		}
+		return sum;
+	}
+
+	int sum2(int... values) {
+		int sum = 0;
+		for (int value : values) {
+			sum += value;
+		}
+		return sum;
+	}
+}
