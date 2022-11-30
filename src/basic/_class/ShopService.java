2480e549d88cfdcf56c17ced1e7614f5aca163a9
rara-dev <kksr1219@gmail.com>
2022-12-01 00:12:26 +0900

    study class

---
+++ src/basic/_class/ShopService.java
@@ -0,0 +1,11 @@
+package basic._class;
+
+public class ShopService {
+	private static ShopService shopService = new ShopService();
+
+	private ShopService() {}
+
+	public static ShopService getInstance() {
+		return shopService;
+	}
+}
