9d21b253030b5fcb6f1827eea7ed702d57afc2e7
rara-dev <kksr1219@gmail.com>
2022-12-01 14:52:39 +0900

    study inheritance

---
+++ src/basic/inheritance/StudentExample.java
@@ -0,0 +1,11 @@
+package basic.inheritance;
+
+public class StudentExample {
+
+	public static void main(String[] args) {
+		Student student = new Student("김자바", "123456-1234567", 1);
+		System.out.println("name: " + student.name);
+		System.out.println("ssn: " + student.ssn);
+		System.out.println("studentNo: " + student.studentNo);
+	}
+}
