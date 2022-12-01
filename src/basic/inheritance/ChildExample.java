ed3802e624b81325917d40858934bfdd26a862e8
rara-dev <kksr1219@gmail.com>
2022-12-01 15:56:40 +0900

    study constructor

---
+++ src/basic/inheritance/ChildExample.java
@@ -0,0 +1,16 @@
+package basic.inheritance;
+
+public class ChildExample {
+
+	public static void main(String[] args) {
+		Child child = new Child();
+
+		/*생성자 호출 순서
+		1. Parent(String nation) call
+		2. Parent() call
+		3. Child(String name) call
+		4. Child() call
+		 */
+	}
+
+}
