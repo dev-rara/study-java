4ad4aeaea998ae23a2a7d31f603b2128745a4a4a
rara-dev <kksr1219@gmail.com>
2022-12-01 19:32:13 +0900

    study promotion

---
+++ src/basic/inheritance/vehicle_example/Driver.java
@@ -0,0 +1,7 @@
+package basic.inheritance.vehicle_example;
+
+public class Driver {
+	public void drive(Vehicle vehicle) {
+		vehicle.run();
+	}
+}
---
+++ src/basic/inheritance/vehicle_example/Vehicle.java
@@ -0,0 +1,7 @@
+package basic.inheritance.vehicle_example;
+
+public class Vehicle {
+	public void run() {
+		System.out.println("차량이 달립니다.");
+	}
+}
