8c2c7edcb6f77d71759bfacd689d50832a7bab4d
rara-dev <kksr1219@gmail.com>
2022-12-05 10:10:30 +0900

    study abstract class

---
+++ src/basic/abstract_class/Dog.java
@@ -0,0 +1,14 @@
+package basic.abstract_class;
+
+public class Dog extends Animal {
+
+	public Dog() {
+		this.kind = "포유류";
+	}
+
+	@Override
+	public void sound() {
+		System.out.println("멍멍");
+	}
+
+}
