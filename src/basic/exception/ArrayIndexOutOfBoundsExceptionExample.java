eca3f7e0d7681af242d79ec8137d815d4349dc12
rara-dev <kksr1219@gmail.com>
2022-12-13 10:49:47 +0900

    study exception handling

---
+++ src/basic/exception/ArrayIndexOutOfBoundsExceptionExample.java
@@ -0,0 +1,13 @@
+package basic.exception;
+
+public class ArrayIndexOutOfBoundsExceptionExample {
+
+	public static void main(String[] args) {
+		String data1 = args[0];
+		String data2 = args[1];
+
+		System.out.println("args[0]: " + data1);  //ArrayIndexOutOfBoundsException 발생
+		System.out.println("args[1]: " + data2);
+	}
+
+}
