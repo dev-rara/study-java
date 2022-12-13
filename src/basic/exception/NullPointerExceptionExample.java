ceffd85c40deeae0bb25cfa1694c98aeed4ed9e9
rara-dev <kksr1219@gmail.com>
2022-12-13 10:39:47 +0900

    study exception handling

---
+++ src/basic/exception/NullPointerExceptionExample.java
@@ -0,0 +1,9 @@
+package basic.exception;
+
+public class NullPointerExceptionExample {
+
+	public static void main(String[] args) {
+		String data = null;
+		System.out.println(data.toString());   //NullPointerException 발생
+	}
+}
